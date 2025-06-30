ace2b9c6105cea0d836c75541af9ead7ff454132
Amr-tmorot <88274242+Amr-tmorot@users.noreply.github.com>
2025-07-01 01:47:23 +0300

    Update main.go

--- main.go
+++ main.go
@@ -17,9 +17,14 @@ import (
 
 func main() {
 
-	connStr := "host=db user=dev password=dev sslmode=disable"
-	// connStr := "user=dev password=dev sslmode=disable"
+	host := os.Getenv("DB_HOST")
+	port := 5432
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
 
+	connStr := "host=%s port=%d user=%s password=%s dbname=%s sslmode=disable, host, port, user, password, dbname"
+	
 	db, err := sqlx.Open("postgres", connStr)
 
 	if err = db.Ping(); err != nil {
